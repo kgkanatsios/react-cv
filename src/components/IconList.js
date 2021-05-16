68e24191bca065ffa5765cd87c30e01c8eeea3f2
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2021-05-16 23:09:54 +0300

    Create IconList component

---
+++ src/components/IconList.js
@@ -0,0 +1,17 @@
+import React from "react"
+import _ from "lodash"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+function IconList(props) {
+  const icons = _.get(props, "icons", [])
+  return (
+    <div className="icons-list-container d-flex justify-content-between mb-1">
+      {icons &&
+        icons.map((icon, idx) => {
+          return <FontAwesomeIcon icon={icon} key={idx} />
+        })}
+    </div>
+  )
+}
+
+export default IconList
