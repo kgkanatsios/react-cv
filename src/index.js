2eb0e2faafd087bb26fe557311af79699173a864
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2021-05-16 13:43:23 +0300

    Load data and add fontawesome

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style/global.scss';
-import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style/global.scss"
+import App from "./components/App"
+import reportWebVitals from "./reportWebVitals"
+
+import data from "./data/data.json"
+
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+
+library.add(fas, fab)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data={data} />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
