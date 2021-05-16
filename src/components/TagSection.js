f09aa3b7ac339b40e8ed31e7e502904c86d068aa
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2021-05-16 22:54:37 +0300

    Create TagSection component

---
+++ src/components/TagSection.js
@@ -0,0 +1,23 @@
+import React from "react"
+import _ from "lodash"
+
+import TagList from "./TagList"
+
+function TagSection(props) {
+  const tagsList = _.get(props, "tagsList", [])
+  return (
+    <div className="mb-1 tag-section-container">
+      {props.title && (
+        <div className="mb-1 pb-1 border-bottom font-weight-bold tag-section-title">
+          {props.title}
+        </div>
+      )}
+      {tagsList &&
+        tagsList.map((tagList, idx) => {
+          return <TagList {...tagList} key={idx} />
+        })}
+    </div>
+  )
+}
+
+export default TagSection
