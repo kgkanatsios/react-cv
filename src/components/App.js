9d3d39f5ebfa75ca0112c639caf0e20be5a424b6
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2021-05-16 21:53:35 +0300

    Add sidebar to main layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 import _ from "lodash"
+import { Container, Row, Col } from "react-bootstrap"
+
 import Header from "./Header"
 import Section from "./Section"
 
@@ -12,13 +14,27 @@ function App(props) {
     return <Header {...headerData} />
   }
   return pages.map((page, idx) => {
-    const sections = _.get(page, "mainColumn.sections", [])
+    const mainColumnSections = _.get(page, "mainColumn.sections", [])
+    const sidebarSections = _.get(page, "sidebar.sections", [])
     return (
       <section key={idx} className="sheet">
         {page.showHeader && header()}
-        {sections.map((section, idx) => {
-          return <Section {...section} key="idx" />
-        })}
+        <Container fluid>
+          <Row>
+            <Col xs={_.get(page, "hasSidebar") ? 8 : 12}>
+              {mainColumnSections.map((section, idx) => {
+                return <Section {...section} key={idx} />
+              })}
+            </Col>
+            {_.get(page, "hasSidebar") && (
+              <Col xs={4}>
+                {sidebarSections.map((section, idx) => {
+                  return <Section {...section} key={idx} />
+                })}
+              </Col>
+            )}
+          </Row>
+        </Container>
       </section>
     )
   })
