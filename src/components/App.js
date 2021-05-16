4d3790ac080ffca190d08bed582c4085220803f5
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2021-05-16 17:00:03 +0300

    Update App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
 import React from "react"
 import _ from "lodash"
 import Header from "./Header"
+import Section from "./Section"
 
 function App(props) {
-  return (
-    <section className="sheet">
-      {_.get(props.data, "header") && (
-        <Header
-          name={_.get(props.data, "header.name")}
-          title={_.get(props.data, "header.title")}
-          links={_.get(props.data, "header.links")}
-        />
-      )}
-    </section>
-  )
+  const pages = _.get(props.data, "pages", [])
+  const header = () => {
+    const headerData = _.get(props.data, "header")
+    if (!headerData) return
+
+    return <Header {...headerData} />
+  }
+  return pages.map((page, idx) => {
+    const sections = _.get(page, "mainColumn.sections", [])
+    return (
+      <section key={idx} className="sheet">
+        {page.showHeader && header()}
+        {sections.map((section, idx) => {
+          return <Section {...section} key="idx" />
+        })}
+      </section>
+    )
+  })
 }
 
 export default App
